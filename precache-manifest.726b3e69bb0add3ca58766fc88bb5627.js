e2f4afe292ee7176541ca112dbd67581a93769fd
Aashish Gaikwad <aashish.gaikwad@steerlean.com>
2019-05-24 17:35:00 +0530

    Updates

---
+++ precache-manifest.726b3e69bb0add3ca58766fc88bb5627.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b28fa07e29cebe5ee390b6d13f18421",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "49e6ed8721b271de7685",
+    "url": "/myapp/static/css/main.9b21340a.chunk.css"
+  },
+  {
+    "revision": "09f9a990c245582aa496",
+    "url": "/myapp/static/js/2.1f3404aa.chunk.js"
+  },
+  {
+    "revision": "49e6ed8721b271de7685",
+    "url": "/myapp/static/js/main.a2a82caa.chunk.js"
+  },
+  {
+    "revision": "f56cbfd09050d46037d5",
+    "url": "/myapp/static/js/runtime~main.e4f53ea3.js"
+  }
+]);
